a414f132df4f2492779b3f8b0863bb3728326019
BoweiWei <bowei_wei@brown.edu>
2018-02-22 15:38:28 -0500

    hw2 created

---
+++ hw2.R
@@ -0,0 +1,12 @@
+## A template for submitting your homework
+
+## include libraries available from  CRAN only
+require(ggplot2) == T || install.packages("ggplot2", repos="https://cloud.r-project.org/")
+
+
+##  Do not source any other files, because only one file is allowed
+
+## Include the names of all team members
+authors <- function() {
+    c("Xi Luo", "Rossi Luo")
+}
