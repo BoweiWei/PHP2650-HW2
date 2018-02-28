6c39ca1f46cabf5b295ddb7ef26c3c08cf1b4afa
BoweiWei <bowei_wei@brown.edu>
2018-02-28 12:12:17 -0500

    process matrix bug still exist

--- hw2.R
+++ hw2.R
@@ -8,5 +8,27 @@ require(ggplot2) == T || install.packages("ggplot2", repos="https://cloud.r-proj
 
 ## Include the names of all team members
 authors <- function() {
-    c("Xi Luo", "Rossi Luo")
+    c("Zhicong Chu", "Bowei Wei")
 }
+
+## Question 1
+## find files
+files <- dir(pattern="sub.+\\.txt$")
+
+## Create the function for Fisher's Z transform
+Fcal <- function(anum) {
+  return(log((1+anum)/(1-anum))/2)
+}
+
+
+re <- NULL
+re.matrix <- NULL
+for (i in seq_along(files)) {
+  f <- files[i]
+  d <- read.csv(f, header=F, sep = " ")
+##  calculate matrix
+  cor.m <- cor(d)
+  re[[i]]<- matrix(mapply(Fcal, cor.m), c(15,15))
+  re[[i]][is.infinite(re[[i]])] = 1
+  }
+
